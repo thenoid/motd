c6023f4ae0f67ff5c87690a6d6254564294090c7
thenoid <rocky@the-olsens.us>
2015-05-11 01:54:41 -0500

    bleh

--- recipes/updates.rb
+++ recipes/updates.rb
@@ -1,6 +1,12 @@
 
 case node['platform_family'].to_s
 when 'rhel'
+  if (node['motd']['cache_clear'].to_i + 24 * 60 * 60 ) < Time.now.to_i
+    cmd = %w(yum clean all).join(' ')
+    z = Mixlib::ShellOut.new(cmd)
+    z.run_command
+    node.default['motd']['cache_clear'] = Time.now
+  end
   cmd = %w(yum updateinfo summary).join(' ')
   z = Mixlib::ShellOut.new(cmd)
   z.run_command
